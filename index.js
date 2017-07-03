afdcd040094cf567cdf5138eeccd223ab9e2a1cb
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-07-03 16:51:03 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var even = true;
+
+$('.game--cell').one('click', function() {
+  var i;
+  even ? i = 0: i = 1;
+
+  var el = $(this).children()[i];
+  var style = getComputedStyle(el);
+  console.log(style.visibility);
+  el.style.visibility = 'visible';
+
+  even = !even;
+});
